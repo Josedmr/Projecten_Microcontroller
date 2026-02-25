fedadddc6b4afd58b6b21d4b149357cf29ece1d4
Josedmr <r1048530@ucll.be>
2026-02-25 15:06:43 +0100

    myADC

--- components/myGPIO/include/myGPIO.h
+++ components/myGPIO/include/myGPIO.h
@@ -2,16 +2,15 @@
 #define MYGPIO_H
 
 #define LED 2
-
 #define KNOP 5 
 
-void MyGPIO_setup_output(int pin);
+void MyGPIO_setup_led(int pin);
 void MyGPIO_setup_button(int pin);
 
-int MyGPIO_read(int pin);
+int MyGPIO_lezen(int pin);
 int MyGPIO_flank(int pin);
 
-void MyGPIO_write(int pin, int value);
+void MyGPIO_schrijven(int pin, int value);
 void MyGPIO_toggle(int pin);
 
 
--- main/main.c
+++ main/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "myGPIO.h"
+#include "myADC.h"
 
-void app_main(void)
+/*void app_main(void)
 {
     MyGPIO_setup_led(LED);
     MyGPIO_setup_button(KNOP);
@@ -17,4 +17,20 @@ void app_main(void)
 
           vTaskDelay(pdMS_TO_TICKS(10));
     }
+}*/
+
+void app_main(void)
+{
+    myADC_setup();
+
+    while (1)
+    {
+        int waarde = myADC_waarde();
+        printf("ADC waarde: %d\n", waarde);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
+
+
+
