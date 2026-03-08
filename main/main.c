33dd17710bef21776bf6ac8bf5d67054d686db33
Josedmr <r1048530@ucll.be>
2026-03-08 21:11:55 +0100

    myPWM

--- main/main.c
+++ main/main.c
@@ -2,12 +2,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "myADC.h"
+#include "myPWM.h"
 
 /*void app_main(void)
 {
     MyGPIO_setup_led(LED);
     MyGPIO_setup_button(KNOP);
-
+ik gebruik 
     while(1)
     {
          if(MyGPIO_flank(KNOP))
@@ -19,7 +20,7 @@
     }
 }*/
 
-void app_main(void)
+/*void app_main(void)
 {
     myADC_setup();
 
@@ -30,7 +31,24 @@ void app_main(void)
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
+}*/
+
+void app_main(void)
+{
+    myADC_setup();
+    pwm_setup();
+
+    while (1)
+    {
+        int waarde = myADC_waarde();
+        printf("ADC waarde: %d\n", waarde);
+
+        pwm_set(waarde / 4);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
+
 
 
 
